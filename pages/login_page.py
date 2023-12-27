b15e61a704ffb9679b5f935347c1a35b38974176
Вероника Кушнерук <derbilla@mail.ru>
2023-12-27 17:58:41 +0300

    added checks with selectors

--- pages/login_page.py
+++ pages/login_page.py
@@ -7,14 +7,14 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
+        # реализуйте проверку, что есть форма логина
+        # реализуйте проверку, что есть форма регистрации на странице
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "It isn't login url"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login Form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register Form is not presented"

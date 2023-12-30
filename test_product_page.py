5e391a6d7d91b16f5496a0cfd1e4fa0e06e2c2ac
Вероника Кушнерук <derbilla@mail.ru>
2023-12-30 16:05:19 +0300

    found xfail with parametrize

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from pages.product_page import ProductObject
+import pytest
    
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -8,4 +9,29 @@ def test_guest_can_add_product_to_basket(browser):
     page.check_name_of_product("The shellcoder's handbook")
     page.press_submit_button()
     page.solve_quiz_and_get_code()
+
+def test_guest_can_add_product_to_basket2(browser):
+    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductObject(browser, link)
+    page.open()
+    page.check_price("£19.99")
+    page.check_name_of_product("Coders at Work")
+    page.press_submit_button()
+    page.solve_quiz_and_get_code()
+    page.check_add_of_product("Coders at Work")
     
+@pytest.mark.parametrize('promo_code',
+                          ["1,2,3,4,5,6",
+                                  pytest.param("7", marks=pytest.mark.xfail),
+                                  "8,9"])
+def test_guest_can_add_product_to_basket3(browser, promo_code):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{}'.format(promo_code)
+    page = ProductObject(browser, link)
+    page.open()
+    page.check_price("£19.99")
+    page.check_name_of_product("Coders at Work")
+    page.press_submit_button()
+    page.solve_quiz_and_get_code()
+    page.check_add_of_product("Coders at Work")
+    
+
